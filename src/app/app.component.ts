1495f5dd692b86ebf014545b3db6e7dd6228fd3d
frankwang <wangsen9108@gmail.com>
2020-08-15 18:17:08 +0800

    feat(nav): init navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'weather';
+
+  public showMenu: boolean;
+
+  public darkModeActive: boolean;
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.darkModeActive = !this.darkModeActive;
+  }
 }
